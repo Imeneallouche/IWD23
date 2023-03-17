171c2ca87fffd3417d788ce463f8bb0842b92132
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-03-17 16:20:05 +0100

    sync connection has been initialized

--- Server/Backend/assistance/syncChat.py
+++
@@ -1,5 +0,0 @@
-from models import Chat, Room
-import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth.models import User
-from asgiref.sync import sync_to_async, async_to_sync
