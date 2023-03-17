2f6e865d655ecb5176d6b1ccbc9183a731e19bf4
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-03-17 19:33:57 +0100

    chargily backend has been added version 1.0

--- Server/Backend/payment/apps.py
+++ Server/Backend/payment/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'payment'
+class ChargilyAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "chargily_epay_django"
